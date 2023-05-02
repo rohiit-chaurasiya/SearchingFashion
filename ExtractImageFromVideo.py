ef4a84b4227467059feec00eaa84d820e87e3e19
Rohit Chaurasia <rohiit.chaurasiya@gmail.com>
2023-05-02 16:37:08 +0530

    Update ExtractImageFromVideo.py

--- ExtractImageFromVideo.py
+++ ExtractImageFromVideo.py
@@ -23,13 +23,13 @@ video = cv2.VideoCapture(video_path)
 # Get the frames per second (fps) of the video
 fps = video.get(cv2.CAP_PROP_FPS)
 
-# Set the frame interval (in seconds) to extract frames from
+
 frame_interval = 1  # extract one frame per second
 
 # Get the total number of frames in the video
 num_frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
 
-# Create a folder to store the extracted clothing items
+
 output_folder = "clothing_items"
 if not os.path.exists(output_folder):
     os.makedirs(output_folder)
@@ -37,7 +37,7 @@ if not os.path.exists(output_folder):
 # Keep track of clothing items that have already been extracted
 extracted_items = set()
 
-# Loop through the video frames and extract the detected clothing items
+
 for i in range(0, num_frames, int(fps * frame_interval)):
     # Set the current frame position
     video.set(cv2.CAP_PROP_POS_FRAMES, i)
@@ -46,14 +46,14 @@ for i in range(0, num_frames, int(fps * frame_interval)):
     ret, frame = video.read()
 
     if ret:
-        # Apply the transformation pipeline to the frame
+        
         input_image = transform(frame)
 
-        # Run the model on the input image
+       
         with torch.no_grad():
             predictions = model([input_image])
 
-        # Extract the predicted bounding boxes and labels from the model output
+       
         boxes = predictions[0]['boxes'].cpu().numpy()
         labels = predictions[0]['labels'].cpu().numpy()
 
@@ -66,9 +66,9 @@ for i in range(0, num_frames, int(fps * frame_interval)):
                 # Compute the hash of the clothing item to check if it has already been extracted
                 clothing_item_hash = hashlib.sha256(clothing_item.copy()).hexdigest()
                 if clothing_item_hash in extracted_items:
-                    continue  # skip clothing item if it has already been extracted
+                    continue  
 
-                # Add the hash of the clothing item to the set of extracted items
+                
                 extracted_items.add(clothing_item_hash)
 
                 clothing_item_filename = os.path.join(output_folder, "clothing_item_{}_{}_{}.jpg".format(i, j, label))
